fb44ecb484662337f33bf01b285c549496162366
Ирина Сумарокова <irina.sumarokova92@yandex.ru>
2021-10-23 19:38:04 +0300

    Добавьте файлы проекта.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение ");
+            string startString = Console.ReadLine();
+            startString = startString.Replace(" ", "");
+            startString = startString.ToLower( );
+            string resultString = "";
+            
+            for (int i = startString.Length - 1; i >= 0; i--)
+            {
+                resultString += startString[i];
+            }
+
+            Console.WriteLine();
+            if (resultString == startString)
+            {
+                Console.WriteLine("Введенная строка - палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Введенная строка - не палиндром");
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Занятие 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятие_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение ");
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            string resultString = "";
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (stringArray[i].Length > resultString.Length)
+                    resultString = stringArray[i];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Самое длинное слово - {0}", resultString);
+            Console.ReadKey();
+        }
+    }
+}
